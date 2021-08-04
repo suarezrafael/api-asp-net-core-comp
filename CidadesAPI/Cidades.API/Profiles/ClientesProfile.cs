f12bf7765da9638be2ba8d69d8cc639100804843
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-04 14:27:09 -0300

    criado ClientesProflile para uso do AutoMapper

---
+++ CidadesAPI/Cidades.API/Profiles/ClientesProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Cidades.API.Helpers;
+
+namespace Cidades.API.Profiles
+{
+    public class ClientesProfile : Profile
+    {
+        public ClientesProfile()
+        {
+            CreateMap<Entities.Cliente, Models.ClienteDto>()
+             .ForMember(
+                dest => dest.Idade,
+                opt => opt.MapFrom( src => src.DataDeNascimento.GetCurrentAge()));
+        }
+    }
+}
