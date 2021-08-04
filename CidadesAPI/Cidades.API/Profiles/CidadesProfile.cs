c7463b079719f35bdc21bfd28c823ff7d053bfeb
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-04 14:30:26 -0300

    Criado CidadesProfiles para uso do AutoMapper

---
+++ CidadesAPI/Cidades.API/Profiles/CidadesProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cidades.API.Profiles
+{
+    public class CidadesProfile : Profile
+    {
+        public CidadesProfile()
+        {
+            CreateMap<Entities.Cidade, Models.CidadeDto>();
+        }
+    }
+}
