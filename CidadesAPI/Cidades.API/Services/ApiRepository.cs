3353274a7d0737e874c17f0be4e116f44ebc51f1
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-04 16:39:28 -0300

    Adicionado suporte a XML response, remividos using não usados

--- CidadesAPI/Cidades.API/Services/ApiRepository.cs
+++ CidadesAPI/Cidades.API/Services/ApiRepository.cs
@@ -3,7 +3,6 @@ using Cidades.API.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Cidades.API.Services
 {
--- CidadesAPI/Cidades.API/Startup.cs
+++ CidadesAPI/Cidades.API/Startup.cs
@@ -23,7 +23,11 @@ namespace Cidades.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+
+            }).AddXmlDataContractSerializerFormatters();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
