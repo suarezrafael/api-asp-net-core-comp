5f32d6e575037f8a0e9791ce18472411fd490303
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-05 14:04:35 -0300

    Anotações Required para Dto de Criacao de cidade

--- CidadesAPI/Cidades.API/Models/CidadeParaCriacaoDto.cs
+++ CidadesAPI/Cidades.API/Models/CidadeParaCriacaoDto.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Cidades.API.Models
 {
     public class CidadeParaCriacaoDto
     {
-        public Guid Id { get; set; }
-
+        [Required(ErrorMessage = "Você deve informar um Nome.")]
+        [MaxLength(100)]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Você deve informar um Estado.")]
+        [MaxLength(2)]
         public string Estado { get; set; }
 
     }
