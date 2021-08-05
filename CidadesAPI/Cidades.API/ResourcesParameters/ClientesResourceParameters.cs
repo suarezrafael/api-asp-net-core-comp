1e16bd904c188beb42281ba3b260279b853cf858
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-05 14:35:58 -0300

    Correção do nome do parametro de ClientesResourceParameters

--- CidadesAPI/Cidades.API/ResourcesParameters/ClientesResourceParameters.cs
+++ CidadesAPI/Cidades.API/ResourcesParameters/ClientesResourceParameters.cs
@@ -2,6 +2,6 @@
 {
     public class ClientesResourceParameters
     {
-        public string NomeNomeCompleto { get; set; }        
+        public string NomeCompleto { get; set; }        
     }
 }
